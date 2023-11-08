625f59d989a3ecc655f2f57b6fdc519a61c22981
phelippemichel <phelippe.michel@gmail.com>
2023-11-08 18:01:23 -0300

    feat: add JWT to auth

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -1,6 +1,8 @@
 ﻿namespace Blog;
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "9f5f6aa778854f2999f91c9e40f81f5c";
+    public static string JwtKey = "9f5f6aa778854f2999f91c9e40f81f5c";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_test334";
 }
 
