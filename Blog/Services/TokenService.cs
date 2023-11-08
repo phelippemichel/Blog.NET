58de1e0e0595ecebbe8cc8feaab5fcf18a9b4f0e
phelippemichel <phelippe.michel@gmail.com>
2023-11-08 18:06:11 -0300

    feat: change method signing credentials

--- Blog/Services/TokenService.cs
+++ Blog/Services/TokenService.cs
@@ -1,30 +1,28 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
-using Blog.Models;
 using System.Security.Claims;
+using System.Text;
+using Blog.Extensions;
+using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Blog.Services;
 
 public class TokenService
 {
-  public string GenerateToken(User user)
+    public string GenerateToken(User user)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-        var tokenDescription = new SecurityTokenDescriptor
+        var claims = user.GetClaims();
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-            new (ClaimTypes.Name, "phelippe"),
-            new (ClaimTypes.Role, "user"),
-            new (ClaimTypes.Role, "admin"),
-            }),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(8),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), 
-            SecurityAlgorithms.HmacSha256Signature)
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
         };
-        var token = tokenHandler.CreateToken(tokenDescription);
+        var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
     }
 }
