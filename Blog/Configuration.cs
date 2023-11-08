8e6559a9c9ef5f63f83965da41a85833ab1037d0
phelippemichel <phelippe.michel@gmail.com>
2023-11-08 18:50:21 -0300

    feat: struct appsettings

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -4,5 +4,13 @@ public static class Configuration
     public static string JwtKey = "9f5f6aa778854f2999f91c9e40f81f5c";
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "curso_api_test334";
-}
+    public static SmtpConfiguration Smtp = new();
 
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
