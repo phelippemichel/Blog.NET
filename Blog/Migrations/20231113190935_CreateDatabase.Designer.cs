bb90bd32c146f655c164798e14ce617c0f52ad2d
phelippemichel <phelippe.michel@gmail.com>
2023-11-13 17:50:46 -0300

    refactor: new db to course

--- Blog/Migrations/20231113190935_CreateDatabase.Designer.cs
+++ Blog/Migrations/20231113190935_CreateDatabase.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blog.Migrations
 {
     [DbContext(typeof(BlogDataContext))]
-    [Migration("20231107203918_CreateDatabase")]
+    [Migration("20231113190935_CreateDatabase")]
     partial class CreateDatabase
     {
         /// <inheritdoc />
